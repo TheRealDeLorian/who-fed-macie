a6461afb8f4d8ca838c878cc8df9c5b2f0dabca6
Dorian Cottle <thenewdorian21@gmail.com>
2025-08-09 00:17:35 -0600

    goitta gi ti ved

--- wfm-client/app/(tabs)/index.tsx
+++ wfm-client/app/(tabs)/index.tsx
@@ -1,24 +1,60 @@
 import { Image } from "expo-image";
-import { Platform, StyleSheet, TouchableOpacity } from "react-native";
+import { Platform, StyleSheet, TouchableOpacity, View } from "react-native";
 
 import { DoggieEmoji } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { useEffect, useState } from "react";
+
+type FeedingInfo = {
+  personWhoFedMacie: string;
+  timeFed: Date;
+};
 
 export default function HomeScreen() {
+  const [feedingInfo, setFeedingInfo] = useState<FeedingInfo>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    try{
+
+      const getWhoFedMacie = async () => {
+        const response = await fetch("http://linuxserver:8084/whofedmacie");
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        const newFeedingInfo = await response.json();
+        setFeedingInfo(newFeedingInfo);
+      }
+      catch {
+        
+      }
+    };
+
+    getWhoFedMacie();
+  }, []);
+  // {
+  //   //TODO: replace this with the newest person from the api
+  //   personWhoFedMacie: "Joe",
+  //   timeFed: new Date(2025, 8, 8, 23, 54),
+  // }
+
   function handleFeedingMacie() {
     //send api call to the wfm api
-    fetch("http://linuxserver:8084/feedmacie", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstParam: "fedMacie",
-      }),
-    });
+    // fetch("http://linuxserver:8084/feedmacie", {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     firstParam: "fedMacie",
+    //   }),
+    // });
+    console.log("fed macie!");
+    alert("fed macie!");
   }
 
   return (
@@ -35,14 +71,17 @@ export default function HomeScreen() {
         <ThemedText type="title">Who fed Macie?</ThemedText>
         <DoggieEmoji />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+      <ThemedText>
+        Macie was last fed by ____ ____ hours ago at ______.
+      </ThemedText>
+      <ThemedView style={styles.buttonContainer}>
         <TouchableOpacity
           onPress={() => {
             handleFeedingMacie();
           }}
           style={styles.button}
         >
-          I fed Macie!
+          <ThemedText>I fed Macie!</ThemedText>
         </TouchableOpacity>
       </ThemedView>
     </ParallaxScrollView>
@@ -55,7 +94,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 8,
   },
-  stepContainer: {
+  buttonContainer: {
     gap: 8,
     marginBottom: 8,
   },
@@ -67,7 +106,11 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   button: {
-    color: "#224ee0ff",
+    color: "#000000",
     borderRadius: 5,
+    backgroundColor: "#224ee0ff",
+    padding: 20,
+    maxWidth: 200,
+    alignItems: "center",
   },
 });
